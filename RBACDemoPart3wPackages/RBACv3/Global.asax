70973abdb8d0501235e4dde85074bb77ad630e3d
err <rtt@gmail.com>
2019-01-30 16:37:47 +0530

    First Commit

---
+++ RBACDemoPart3wPackages/RBACv3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RBACv3.MvcApplication" Language="C#" %>
---
+++ RBACDemoPart3wPackages_SMM/RBACDemoPart3wPackages/RBACv3/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RBACv3.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult About()
+        {
+            return View();
+        }
+        
+        [RBAC]
+        public ActionResult Reports()
+        {
+            return View();
+        }
+    }
+}
